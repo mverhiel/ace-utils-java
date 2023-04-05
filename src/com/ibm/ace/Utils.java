2e0ac9814f4d785200ad28a4ca6d11f5061b8aa5
Marc Verhiel <mverhiel@ca.ibm.com>
2023-04-05 07:41:13 -0700

    added comment

--- src/com/ibm/ace/Utils.java
+++ src/com/ibm/ace/Utils.java
@@ -3,7 +3,7 @@ package com.ibm.ace;
 public class Utils {
 	
 	public static String getSystemTimeMilliSeconds() {
-		
+		//this code returns system time ms as a String
 		String value = "-1";
 		
 		value = String.valueOf(System.currentTimeMillis());
